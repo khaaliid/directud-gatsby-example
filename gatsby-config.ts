e3ee8e2327980b256e85f9ccd91833ba003818e5
Khaled El Gohary <Khaled.ElGohary@aggreko.com>
2023-09-02 07:19:15 +0200

    change authentication to be a token based

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,21 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+			resolve: '@directus/gatsby-source-directus',
+			options: {
+				url: `http://127.0.0.1:8055/`, // Fill with your Directus instance address
+				auth: {
+					token: 'cg2AR1p5HDcthtx-bnACVAKd4FoH5b8l', // You can use a static token from an user
+
+					// Or you can use the credentials of an user
+					// email: "admin@example.com",
+					// password: "d1r3ctu5",
+				},
+			},
+		}
+  ],
 }
 
 export default config
